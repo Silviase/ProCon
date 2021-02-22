9b134eccfce59119aea073b98fcb04ec5da024dd
root <root@silviaseDESTKTOP.localdomain>
2021-02-22 11:13:42 +0900

    abc192b

--- abc192/src/bin/b.rs
+++ abc192/src/bin/b.rs
@@ -1,10 +1,24 @@
-use proconio::{input, fastout};
-use ac_library_rs::*;
+use proconio::{input, fastout, marker::Chars};
 
 #[fastout]
 fn main() {
     input! {
-        
+        s: Chars,
     }
-    todo!();
+
+    let mut f = true;
+    let mut g = false;
+
+    for c in s {
+        f &= c.is_ascii_lowercase() ^ g;
+        g = !g;
+    }
+
+    if f {
+        println!("Yes")
+    }else{
+        println!("No")
+    }
+
+
 }
